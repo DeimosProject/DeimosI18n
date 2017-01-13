05e3d371a194b6ea54d362a2a72fab39b8a8d80a
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-13 14:23:08 +0300

    add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","ArrayAccess"],["c","ArrayObject"],["c","Countable"],["c","Deimos\\I18n\\I18n"],["c","IteratorAggregate"],["c","Serializable"],["c","Traversable"]];
